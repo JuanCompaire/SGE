fe55add1bc4aa31daf8b1e11235587041030032d
JuanCompaire <juancompaire2004@gmail.com>
2023-10-27 10:39:03 +0200

    avances modulo examen ejemplo
    Signed-off-by: JuanCompaire <juancompaire2004@gmail.com>

--- odoo-compose/addons/modulo_ejemplo_examen/models.py
+++ odoo-compose/addons/modulo_ejemplo_examen/models.py
@@ -11,5 +11,10 @@ class Turco(models.Model):
     fecha_nacimiento = fields.Date(required=True)
     rango = fields.Char()
 
+class Soldado(models.Model):
+    _name = 'final_fantasy.soldado'
+    fecha_nacimiento = fields.Date(required=True)
+
+
 
     

ff514a36caaca20be63dc11379a62bc40c7763c2
JuanCompaire <juancompaire2004@gmail.com>
2023-11-08 19:19:57 +0100

    añadimos codigo modulo U3A2
    Signed-off-by: JuanCompaire <juancompaire2004@gmail.com>

--- odoo-compose/addons/U3A2/models.py
+++ odoo-compose/addons/U3A2/models.py
@@ -2,12 +2,21 @@ from odoo import api, models, fields
 
 class Cliente(models.Model):
     _name = 'veterinaria.cliente'
-    name = fields.Char()
+    nombre = fields.Char()
     fecha_inscripcion = fields.Date(required=True)
     cod_mascotas = fields.One2many(comodel_name='veterinaria.mascota', inverse_name='cod_cliente')
+    edad = fields.Integer(required=True);
+    
+    @api.constrains('edad','nombre')
+    def _check_edad(self):
+        for record in self:
+            if record.edad < 18:
+                raise models.ValidationError('El cliente ' + record.nombre + ' debe ser mayor de edad, ya que tiene'+ str(record.edad) +' años.')
+            else:
+                pass
 
 class Mascota(models.Model):
     _name = 'veterinaria.mascota'
-    name = fields.Char()
+    nombre = fields.Char()
     tipo = fields.Char()
     cod_cliente = fields.Many2one(comodel_name='veterinaria.cliente', inverse_name='cod_mascotas')

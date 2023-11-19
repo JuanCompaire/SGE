d07573179b832ec123f4c97c5baddf61f3e75c11
JuanCompaire <juancompaire2004@gmail.com>
2023-11-19 18:05:20 +0100

    1 punto finish

--- odoo-compose/addons/FinalFantasy/models.py
+++ odoo-compose/addons/FinalFantasy/models.py
@@ -1,1 +1,7 @@
 from odoo import api, models, fields
+
+class Shinra(models.Model):
+    _name = 'finalfantasy.shinra'
+    _description = 'Shira'
+    name = fields.Char(string="Nombre")
+    fecha_nacimiento = fields.Date(string="Fecha de nacimiento", required=True)

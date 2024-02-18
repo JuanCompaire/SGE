056c95f4778e59f9c2777fee2288344a7f9f9e68
JuanCompaire <juancompaire2004@gmail.com>
2024-02-18 16:51:04 +0100

    para alex

--- salesforce/pika/force-app/main/default/aura/componente1/componente1Controller.js
+++ salesforce/pika/force-app/main/default/aura/componente1/componente1Controller.js
@@ -2,7 +2,7 @@
     djdhfsjdj: function(component, event, helper) {
         },
     saludar : function(component, event, helper) {
-        alert("antes eras "+ component.get("v.nombre")+" y ahora eres Alert!");
+        alert("antes eras "+ component.get("v.nombreOld")+" y ahora eres "+ component.get("v.nombre"));
         component.set("v.nombre", "Alert!");
         //triggers an event
         var evt = $A.get("e.c:evtData");
@@ -11,6 +11,7 @@
             "apellido" : "Alert!",
         };
         evt.setParams(obj);
+        component.set("v.nombreOld", component.get("v.nombre"));
         evt.fire();
     }
 })
